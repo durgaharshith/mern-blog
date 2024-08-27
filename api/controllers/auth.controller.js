a84fb9906d6d1d2a4b9a6836e886940ddc9a9aa4
durgaharshith <durgaharshithpigili@gmail.com>
2024-08-27 16:09:37 +0530

    completed signup page UI

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -5,7 +5,7 @@ import { errorHandler } from "../utils/error.js";
 export const signup = async (req,res,next) =>{
     const {username,email,password} =req.body;    
     if(!username || !email || !password || username === ''|| email === '' || password === ''){
-        next(errorHandler(400,'All fields are required'));
+        return next(errorHandler(400,'All fields are required'));
     }
 
     const hashedPassword = bcryptjs.hashSync(password,10);
